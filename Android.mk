894ed4af9dd914524ce4967796202fe6a4c77a22
dodyirawan85 <dev.irawans@gmail.com>
2025-10-22 20:45:33 +0700

    uke: Initial vendor tree
    * With latest commit at https://github.com/Xiaomi-Pad-7-Pro-Resources/android_device_xiaomi_uke/commit/a1d4430e5d44a14f701c5e4a216d56242677d540
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,31 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),uke)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,e30ee94543d067dc0f47e5cff133ee85d27c4802)
+$(call add-radio-file-sha1-checked,radio/aop.img,743da5b5508ca695e6b4f54772a689eb27a26b96)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,abd79781720c4fcefe01e8cf24b9b9567ea98527)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,362753f263beb56f9dddbcada3ce584580925855)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,b62d754f73cc7c0f7d8208174129bfff663cbcd3)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,98348f0862ffee51d48978f8ced2d23d58d11123)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,adfd7af4169d6f14f464cd8ddb9590edf01a862d)
+$(call add-radio-file-sha1-checked,radio/dsp.img,64ef1f028d798d3c55db01283cbde8c2d3f2b5d8)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,ce30b4824dc84d33429e3a2b81d95411af81d432)
+$(call add-radio-file-sha1-checked,radio/hyp.img,f164415450df095727451e4883b07306e1757f9d)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,309ebdbd9a045a4e44d16b26d5d0615b3f25c967)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,c8769a516eafefa39743531e446e3b7cc6f0ae20)
+$(call add-radio-file-sha1-checked,radio/modem.img,6b9ab4145ebc044a62499581eae1fb64005de2b6)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,96f03f03b47b0e4b97aba53674eda66181f969cc)
+$(call add-radio-file-sha1-checked,radio/shrm.img,37194baa0987743ebfd2ba4ab808c15932a9c4bc)
+$(call add-radio-file-sha1-checked,radio/tz.img,a8b9da7668de18492dee361137fdf2dcfd644889)
+$(call add-radio-file-sha1-checked,radio/uefi.img,0760a0b2657ecfbea276463b2c21125d1cdaf717)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,243885accc9265c2382117fe9fdb27ccc03ea3ed)
+$(call add-radio-file-sha1-checked,radio/xbl.img,8f6901c1ecb3dc7b9eb6fa23de548384a2b3d5fe)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,996e4cf771b0354e966207413f01be1538370529)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,f935c3ec797f3627ba9d52ba814bd616919a6814)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,26 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    qupfw \
+    shrm \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
