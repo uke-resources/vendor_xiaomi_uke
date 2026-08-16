93f1eee24ff43a686391c6b4cb896de34fe86893
NSR-XINE <nasironfire07@gmail.com>
2026-08-16 17:15:09 +0000

    uke: update from OS3.0.303.0.WOZMIXM

--- Android.mk
+++ Android.mk
@@ -3,29 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),uke)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,ed169426873853b1cf7cd34fbc395b17ce41fbbe)
-$(call add-radio-file-sha1-checked,radio/aop.img,1b4b3bb7db72186649b28963024f669d0489e46c)
-$(call add-radio-file-sha1-checked,radio/aop_config.img,1374e3ab98d82eb42d3e7f2e68b43a507bce5228)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,e8f0152a5edf678f2b4c66250f5f266ca0bd6d26)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,eeffeef6b656f06060b5521a681befccec6d105b)
-$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,5e4ef249386a018db286ef55851c018127250aeb)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,3cd60d745cb6f3b5cc6801c7458c79502ae78e7a)
-$(call add-radio-file-sha1-checked,radio/dsp.img,56d6a77b8d1e8aa6685d992319296cf23b1d63e7)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,e6e3596c78b72bebd76ceceb196b3347f3d14050)
-$(call add-radio-file-sha1-checked,radio/hyp.img,1081e7de8475c2aea900dd5bbbc4c8e801f9f8a5)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,4f28aa648342a86918096090bbeaf2e6bb7a23e3)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,99751b8bfa9646f481bcd2272cbc10a3786f990a)
-$(call add-radio-file-sha1-checked,radio/modem.img,4dec8e9bc02c03737f879f250c02f8f7bbdc4f54)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,c6a36280f34f0378a91c9ff8c3797a2f4096ecc2)
-$(call add-radio-file-sha1-checked,radio/shrm.img,1df824f77995f5c2444ed06fa566a045fb8b0742)
-$(call add-radio-file-sha1-checked,radio/tz.img,7c9981059620f8e1fd2e6b14f3f0a2313fbf909c)
-$(call add-radio-file-sha1-checked,radio/uefi.img,9d4b983db1b252fe8ee43af609988ef0d5e669c2)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,9d1dc2dec0829f90ca65c2c37dd1f43d36681f47)
-$(call add-radio-file-sha1-checked,radio/xbl.img,3f1d00be3fc078f606b80ea58f9a8c9231f43b50)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,15475c74d35ce264bd6d65ef1ed121c992e3647f)
-$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,943642db8f2f9a42ec114919f94e85d6e4b7a83b)
-
-endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,26 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    aop_config \
-    bluetooth \
-    cpucp \
-    cpucp_dtb \
-    devcfg \
-    dsp \
-    featenabler \
-    hyp \
-    imagefv \
-    keymaster \
-    modem \
-    qupfw \
-    shrm \
-    tz \
-    uefi \
-    uefisecapp \
-    xbl \
-    xbl_config \
-    xbl_ramdump
